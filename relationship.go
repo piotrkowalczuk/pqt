487f9e557305c67eb25375927a8cf79f64f05ba7
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2016-01-15 01:54:05 +0100

    even more empty comments

--- relationship.go
+++ relationship.go
@@ -10,8 +10,10 @@ const (
 	RelationshipTypeManyToManySelfReferencing
 )
 
+// RelationshipType ...
 type RelationshipType int
 
+// Relationship ...
 type Relationship struct {
 	Type                                  RelationshipType
 	MappedBy, InversedBy                  string
@@ -32,42 +34,51 @@ func newRelationship(t, j *Table, rt RelationshipType, opts ...relationshipOpt) 
 	return r
 }
 
+// OneToOneUnidirectional ...
 func OneToOneUnidirectional(t *Table, opts ...relationshipOpt) *Relationship {
 	return newRelationship(t, nil, RelationshipTypeOneToOneUnidirectional, opts...)
 }
 
+// OneToOneBidirectional ...
 func OneToOneBidirectional(t *Table, opts ...relationshipOpt) *Relationship {
 	return newRelationship(t, nil, RelationshipTypeOneToOneBidirectional, opts...)
 }
 
+// OneToOneSelfReferencing ...
 func OneToOneSelfReferencing(opts ...relationshipOpt) *Relationship {
 	return newRelationship(nil, nil, RelationshipTypeOneToOneSelfReferencing, opts...)
 }
 
+// OneToMany ...
 func OneToMany(t *Table, opts ...relationshipOpt) *Relationship {
 	return newRelationship(t, nil, RelationshipTypeOneToMany, opts...)
 }
 
+// OneToManySelfReferencing ...
 func OneToManySelfReferencing(t *Table, opts ...relationshipOpt) *Relationship {
 	return newRelationship(t, nil, RelationshipTypeOneToManySelfReferencing, opts...)
 }
 
+// ManyToMany ...
 func ManyToMany(t, j *Table, opts ...relationshipOpt) *Relationship {
 	return newRelationship(t, j, RelationshipTypeManyToMany, opts...)
 }
 
+// ManyToManySelfReferencing ...
 func ManyToManySelfReferencing(j *Table, opts ...relationshipOpt) *Relationship {
 	return newRelationship(nil, j, RelationshipTypeManyToManySelfReferencing, opts...)
 }
 
 type relationshipOpt func(*Relationship)
 
+// WithMappedBy ...
 func WithMappedBy(s string) relationshipOpt {
 	return func(r *Relationship) {
 		r.MappedBy = s
 	}
 }
 
+// WithInversedBy ...
 func WithInversedBy(s string) relationshipOpt {
 	return func(r *Relationship) {
 		r.InversedBy = s
